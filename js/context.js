e68b600450678127f17af456625ac31c9877fd56
Joe-Z <franz.lanzendorfer@gmx.at>
2011-04-19 12:53:41 +0200

    color is now passed as an uniform-variable

--- js/context.js
+++ js/context.js
@@ -57,6 +57,54 @@ WebGLRenderingContext.prototype.loadShader = function(id) {
 
 };
 
+WebGLRenderingContext.prototype.linkShaderProgram = function(vertexShader, fragmentShader) {
+
+	var shaderProgram = this.createProgram();
+
+    this.attachShader(shaderProgram, vertexShader);
+    this.attachShader(shaderProgram, fragmentShader);
+    
+
+    this.linkProgram(shaderProgram);
+
+
+    if (!this.getProgramParameter(shaderProgram, this.LINK_STATUS)) {
+  
+        log("Unable to initialize the shader program.");
+
+    }
+	
+	return shaderProgram;
+	
+};
+
+WebGLRenderingContext.prototype.setupDefaultShader = function() {
+
+	var vertexShader = this.loadShader("vertex-shader");
+	var fragmentShader = this.loadShader("fragment-shader");
+	
+	this.defaultShader = this.linkShaderProgram(vertexShader, fragmentShader);
+	
+	this.useProgram(this.defaultShader);
+	
+};
+
+/*WebGLRenderingContext.prototype.setColor = function(r, g, b, a) {
+
+	var colors = [
+        1.0, 0, 0, 1.0,
+        0, 0, 0, 1.0,
+        0, 0, 0, 1.0,
+        0, 0, 0, 1.0
+    ];
+
+    colorBuffer = this.createBuffer();
+    
+    this.bindBuffer(this.ARRAY_BUFFER, colorBuffer);
+    this.bufferData(this.ARRAY_BUFFER, new Float32Array(colors), this.STATIC_DRAW);
+
+};*/
+
 WebGLRenderingContext.prototype.drawRect = function(x, y, width, height) {
     
     var vertexBuffer = this.createBuffer();
@@ -71,53 +119,20 @@ WebGLRenderingContext.prototype.drawRect = function(x, y, width, height) {
     ];
 
     this.bufferData(this.ARRAY_BUFFER, new Float32Array(vertices), this.STATIC_DRAW);
+	
+	var vertexPositionAttribute = this.getAttribLocation(this.defaultShader, "position");
+    this.enableVertexAttribArray(vertexPositionAttribute);
     
-    var colors = [
-        1.0, 0, 0, 1.0,
-        0, 0, 0, 1.0,
-        0, 0, 0, 1.0,
-        0, 0, 0, 1.0
+    this.vertexAttribPointer(vertexPositionAttribute, 3, this.FLOAT, false, 0, 0);
+	
+	var colors = [
+        1.0, 0, 0, 1.0
     ];
 
-    colorBuffer = this.createBuffer();
-    
-    this.bindBuffer(this.ARRAY_BUFFER, colorBuffer);
-    this.bufferData(this.ARRAY_BUFFER, new Float32Array(colors), this.STATIC_DRAW);
-    
-    var vertexShader = this.loadShader("vertex-shader");
-    var fragmentShader = this.loadShader("fragment-shader");
+    var colorUniform = this.getUniformLocation(this.defaultShader, "color");
+    this.uniform4fv(colorUniform, new Float32Array(colors));
 
-    var shaderProgram = this.createProgram();
-
-    this.attachShader(shaderProgram, vertexShader);
-    this.attachShader(shaderProgram, fragmentShader);
-    
-
-    this.linkProgram(shaderProgram);
-
-
-    if (!this.getProgramParameter(shaderProgram, this.LINK_STATUS)) {
-  
-        alert("Unable to initialize the shader program.");
-
-    }
-
-    this.useProgram(shaderProgram);
-
-    var vertexPositionAttribute = this.getAttribLocation(shaderProgram, "position");
-    this.enableVertexAttribArray(vertexPositionAttribute);
-
-    var vertexColorAttribute = this.getAttribLocation(shaderProgram, "color");
-    this.enableVertexAttribArray(vertexColorAttribute);
-    
-    
-    this.bindBuffer(this.ARRAY_BUFFER, vertexBuffer);
-    this.vertexAttribPointer(vertexPositionAttribute, 3, this.FLOAT, false, 0, 0);
-
-    this.bindBuffer(this.ARRAY_BUFFER, colorBuffer);
-    this.vertexAttribPointer(vertexColorAttribute, 4, this.FLOAT, false, 0, 0);
-
-
+	
     var matrix = [
         1, 0, 0, 0,
         0, 1, 0, 0,
@@ -125,7 +140,7 @@ WebGLRenderingContext.prototype.drawRect = function(x, y, width, height) {
         0, 0, 0, 1
     ];
 
-    var mUniform = this.getUniformLocation(shaderProgram, "matrix");
+    var mUniform = this.getUniformLocation(this.defaultShader, "matrix");
     this.uniformMatrix4fv(mUniform, false, new Float32Array(matrix));
 
 
