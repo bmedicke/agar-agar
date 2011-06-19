c3181632aee7af607b5aac8b68175f80623d8cc4
Eberhard Gräther <ebsigraether@hotmail.com>
2011-06-19 18:56:26 +0200

    added bind and extend

--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,36 @@
-var log = function() {
+
+function log() {
+    
     console.log.apply(console, arguments);
+    
 };
+
+
+function bind(scope, fn) {
+    
+    return function() {
+        
+        fn.apply(scope, arguments);
+        
+    };
+    
+};
+
+function extend(destination, source) {
+    
+    for (var key in source) {
+        
+        if (source.hasOwnProperty(key)) {
+            
+            destination[key] = source[key];
+            
+        }
+        
+    }
+    
+    return destination;
+};
+
 
 /**
  * Provides requestAnimationFrame in a cross browser way.
@@ -15,11 +45,11 @@ if ( !window.requestAnimationFrame ) {
         window.mozRequestAnimationFrame || // comment out if FF4 is slow (it caps framerate at ~30fps: https://bugzilla.mozilla.org/show_bug.cgi?id=630127)
         window.oRequestAnimationFrame ||
         window.msRequestAnimationFrame ||
-    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+        function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
 
-    window.setTimeout( callback, 1000 / 60 );
+            window.setTimeout( callback, 1000 / 60 );
 
-    };
+        };
 
     } )();
 
